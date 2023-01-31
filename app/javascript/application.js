504fa29c50540eb5d08e4e07cc9808ffa05e6e59
Chris Oliver <excid3@gmail.com>
2023-01-31 09:13:05 -0600

    Confirmation text in confirm modals

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,10 +2,31 @@
 import "@hotwired/turbo"
 import "./controllers"
 
-Turbo.setConfirmMethod((message, element) => {
+Turbo.setConfirmMethod((message, element, button) => {
   console.log(message, element)
+
+  let confirmText = element.dataset.turboConfirmText
+  let description = element.dataset.turboConfirmDescription || ""
+
   let dialog = document.getElementById("turbo-confirm")
-  dialog.querySelector("p").textContent = message
+  let confirmField = dialog.querySelector("[data-behavior='confirm-text']")
+  let commitButton = dialog.querySelector("button[value='confirm']")
+
+  dialog.querySelector("[data-behavior='title']").textContent = message
+  dialog.querySelector("[data-behavior='description']").innerHTML = description
+  confirmField.value = ""
+
+  if (confirmText) {
+    confirmField.style.display = ""
+    commitButton.disabled = true
+
+    confirmField.addEventListener("input", (event) => {
+      commitButton.disabled = (event.target.value != confirmText)
+    })
+  } else {
+    confirmField.style.display = "none"
+  }
+
   dialog.showModal()
 
   return new Promise((resolve, reject) => {
